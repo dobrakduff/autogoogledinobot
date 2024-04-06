deaaca7aa491fee74226de737ed56c5c2141b0ce
dobrakduff <159202251+dobrakduff@users.noreply.github.com>
2024-04-06 15:17:25 +0300

    add multiprocessing

--- main.py
+++ main.py
@@ -1,43 +1,70 @@
 import cv2
 import numpy as np
 import pyautogui
+import multiprocessing
 
 # Constants
 ROI_X = 400
-ROI_Y = 550
-ROI_WIDTH = 255
+ROI_Y = 625
+ROI_WIDTH = 250
 ROI_HEIGHT = 250
-THRESHOLD_MIN_B = 5000
-THRESHOLD_MIN_W=1000
-THRESHOLD_MAX = 30000
+THRESHOLD_MAX_B = 59000
+THRESHOLD_MAX_W=10000
+THRESHOLD_MIN_B=10000
+THRESHOLD_MIN_W=1500
 WAIT_KEY_DELAY = 10  # Milliseconds
+jumps_count = 0
 
-while True:
-    # Capture screen
-    screen = pyautogui.screenshot(region=(ROI_X, ROI_Y, ROI_WIDTH, ROI_HEIGHT))
-    frame = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
 
-    # Convert to grayscale
+def screen_capture(queue):
+    while True:
+        screen = pyautogui.screenshot(region=(ROI_X, ROI_Y, ROI_WIDTH, ROI_HEIGHT))
+        frame = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
+        queue.put(frame)
+
+
+def process_frame(frame):
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Count black and white pixels
     black_px_count = np.sum(gray_frame < 100)
-    white_px_count = np.sum(gray_frame > 100)
+    return black_px_count
 
-    # Print pixel counts
-    print(white_px_count, ROI_X,black_px_count)
 
-    # Perform action based on pixel counts
-    if THRESHOLD_MIN_B < black_px_count < THRESHOLD_MAX or THRESHOLD_MIN_W < white_px_count < THRESHOLD_MAX:
-        pyautogui.press("up")
-        ROI_X += 2
+if __name__ == '__main__':
+    # Create a queue to share data between processes
+    queue = multiprocessing.Queue()
 
-    # Display the frame
-    cv2.imshow('frame', frame)
+    # Create and start the screen capture process
+    screen_capture_process = multiprocessing.Process(target=screen_capture, args=(queue,))
+    screen_capture_process.start()
 
-    # Exit if 'q' is pressed
-    if cv2.waitKey(WAIT_KEY_DELAY) & 0xFF == ord('q'):
-        break
+    while True:
+        # Retrieve the captured frame from the queue
+        frame = queue.get()
 
-# Close all windows
-cv2.destroyAllWindows()
+        # Process the frame
+        black_px_count = process_frame(frame)
+
+        # Print pixel counts
+        print("Black Pixel Count:", black_px_count)
+
+        # Perform action based on pixel counts
+        if THRESHOLD_MIN_B < black_px_count < THRESHOLD_MAX_B or THRESHOLD_MIN_W < black_px_count < THRESHOLD_MAX_W:
+            pyautogui.press("up")
+            jumps_count += 1
+
+        # Display the frame
+        cv2.imshow('frame', frame)
+
+        if jumps_count == 5:
+            jumps_count = 0
+            ROI_X += 5
+
+        # Exit if 'q' is pressed
+        if cv2.waitKey(WAIT_KEY_DELAY) & 0xFF == ord('q'):
+            break
+
+    # Terminate the screen capture process
+    screen_capture_process.terminate()
+
+    # Close all windows
+    cv2.destroyAllWindows()
